a11b7beae26c8dc3cc0061dd1302c8a065f44e87
justaneta <anna.pavliuk387@gmail.com>
2023-07-26 22:23:21 +0300

    new

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-part1 = input('Word')
-part2 = 'has 3 letters'
+#part1 = 'enter name'
+#part2= "anna"
+#result= f'Word {part2} has {len(part2)} letters'
+#print(result)
+
+#part1 = 'enter name'
+#part2= 'anna'
+#result= f'Word {part2} has {len(part2)} letters'
+#print(result)
+
+part1= input('Words')
+part2= f"Word {part1} has {len(part1)} letters"
 print(part2)
+
+#part1= 'Word'
+#part2= len(part1)
+#print(part1+ 'has {len(part2)} letters')
