5596d22bf83dafaa287bae6288b9f196a09b4f6b
justaneta <anna.pavliuk387@gmail.com>
2023-08-08 10:43:10 +0300

    task_5

---
+++ home_task5.py
@@ -0,0 +1,20 @@
+#a
+def my_function(first_num, second_num):
+  print(first_num * second_num)
+
+my_function (45, 56)
+
+#b
+def my_function(first_name, last_name):
+  print(first_name + last_name)
+
+my_function("Emili", " Rayan")
+
+#c
+# -
+
+#d
+if __name__ == '__main__':
+    from import_file import my_function
+    my_function(1,2)
+
